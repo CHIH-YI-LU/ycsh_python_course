6eac8365094760a11179914e410bee29bcce4a6f
CHIH-YI-LU <iris934621@gmail.com>
2020-10-20 16:14:10 +0800

    L06 files

---
+++ L06/circle_area.py
@@ -0,0 +1,6 @@
+# 提示：圓周率
+pi = 3.14
+r = float(input("請輸入半徑:"))
+v = pi*r**2
+v = round(v,1)
+print("圓形的面積是:",v)
---
+++ L06/triangle_area.py
@@ -0,0 +1,5 @@
+a = float(input("請輸入三角形的高:"))
+b = float(input("請輸入三角形的底邊長:"))
+v = (a*b)/2
+v = round(v,1)
+print("三角形的面積是:",v)
