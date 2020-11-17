60fe66828841e029fe7fa13075aad7eb2a9f775c
CHIHYILU <iris934621@gmail.com>
2020-11-17 07:24:18 +0000

    update2

---
+++ test/x3.py
@@ -0,0 +1,12 @@
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+e = float(input())
+f = float(input())
+
+h = (e*c-b*f)/(a*e-b*d)
+i = (a*f-d*c)/(a*e-b*d)
+
+print(-h)
+print(-i)
